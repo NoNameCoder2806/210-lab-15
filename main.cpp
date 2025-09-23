a6d002f729a1d4c7753a733d0778aaeb3633ce2f
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-22 23:06:05 -0700

    Implemented readMovies() function

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ int main()
     vector<Movie> movies;
 
     // Read the movies
+    readMovies("input.txt", movies);
 
     return 0;
 }
@@ -39,18 +40,43 @@ void readMovies(string path, vector<Movie>& movies)
     ifstream fin;
     fin.open(path);
 
+    // Check whether the file was successfully opened
+    if (!fin) {
+        cerr << "Error opening file\n";
+        return;
+    }
+
     // Declare a string line variable to store the line
     string line = "";
 
     // Iterate and read the file
-    while (getline(fin, line))
+    while (true)
     {
         // Create a temporary Movie object
         Movie temp;
 
-        // Store the name of the Movie
+        // Read and store the title of the Movie
+        if (!getline(fin, line))    // If we can't read the line, we break
+        {
+            break;
+        }
         temp.setTitle(line);
-        temp.setYearReleased(getline(fin, line));
 
+        // Read and store the released year of the Movie
+        if (!getline(fin, line))    // If we can't read the line, we break
+        {
+            break;
+        }
+        temp.setYearReleased(stoi(line));
+
+        // Read and store the screenwriter of the Movie
+        if (!getline(fin, line))    // If we can't read the line, we break
+        {
+            break;
+        }
+        temp.setScreenWriter(line);
+
+        // Add the Movie object to the vector
+        movies.push_back(temp);
     }
 }
