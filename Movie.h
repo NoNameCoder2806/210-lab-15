732abee34ff4da1817039855603c81bf0c7d7e67
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-22 22:46:52 -0700

    Completed Movie class

--- Movie.h
+++ Movie.h
@@ -5,27 +5,61 @@
 
 // Libraries
 #include <iostream>
+#include <string>
 using namespace std;
 
 // Movie class
 class Movie
 {
 private:
+    string screenWriter;
+    int yearReleased;
+    string title;
 
 public:
     // Accessors / Getters
+    string getScreenWriter()
+    {
+        return screenWriter;
+    }
+
+    int getYearReleased()
+    {
+        return yearReleased;
+    }
+
+    string getTitle()
+    {
+        return title;
+    }
     
     // Mutators / Setters
+    void setScreenWriter(string sw)
+    {
+        screenWriter = sw;
+    }
+
+    void setYearReleased(int yr)
+    {
+        yearReleased = yr;
+    }
+
+    void setTitle(string t)
+    {
+        title = t;
+    }
     
     // Methods
     /*
         print()
-        
+        Display the information of a Movie object
         Arguments: none
         Return: none
     */
     void print()
     {
-        
+        cout << "Movie: " << getTitle() << endl;
+        cout << "   Year released: " << getYearReleased() << endl;
+        cout << "   Screenwriter: " << getScreenWriter() << endl;
     }
 };
