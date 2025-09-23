7319b3ee556ab7935e46f2dee528a348e479a87a
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-22 22:36:00 -0700

    Setting up Lab 15...

---
+++ Movie.h
@@ -0,0 +1,31 @@
+// COMSC-210 | Lab 15 | Dat Hoang Vien
+// IDE used: Visual Code Studio
+
+#pragma once
+
+// Libraries
+#include <iostream>
+using namespace std;
+
+// Movie class
+class Movie
+{
+private:
+
+public:
+    // Accessors / Getters
+    
+    // Mutators / Setters
+    
+    // Methods
+    /*
+        print()
+        
+        Arguments: none
+        Return: none
+    */
+    void print()
+    {
+        
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,22 @@
+// COMSC-210 | Lab 15 | Dat Hoang Vien
+// IDE used: Visual Code Studio
+
+// Libraries
+#include <iostream>
+#include <vector>
+#include "Movie.h"
+using namespace std;
+
+// Constants
+
+// Function prototypes
+
+// Main function
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
+
+// Function implementations
